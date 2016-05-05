a4c0e4e310ca801187c9d56bd98b90306b43f298
joephon <306519779@qq.com>
2016-05-05 17:21:26 +0800

    added

--- myKeys.js
+++
@@ -1,8 +0,0 @@
-export default {
-  AppId: 'lG1fy3zGFvRhxHoIjCnQUiJz-gzGzoHsz',
-  AppKey: 'YwpcQNLj8zlVWxLbSlH2SPX4',
-  AK: 'qwEwcX5AX-F9n9ytKxZenIl8qE-q5rC4AqR6jutR',
-  SK: 'R24cLLBK1-Hrx--F4b2kL0QBWPsUtlt5FsFsovnA',
-  FIR: '76261faf196444d3ba6397f45c317737',
-  V: 1.45,
-}
